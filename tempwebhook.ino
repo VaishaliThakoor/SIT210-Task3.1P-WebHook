96f53da35fc58ec632dc75d047a3fd6b51461f5e
VaishaliThakoor <63847679+VaishaliThakoor@users.noreply.github.com>
2020-06-05 18:56:57 +1000

    Create tempwebhook.ino

---
+++ tempwebhook.ino
@@ -0,0 +1,17 @@
+//I have used mock values for the temperature readings as i do not have a temperature sensor
+int led = D7;  // The on-board LED
+
+void setup() {
+  pinMode(led, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(led, HIGH);   // Turn ON the LED
+
+  String temp = String(random(60, 80));
+  Particle.publish("temp", temp, PRIVATE);
+  delay(30000);               // Wait for 30 seconds
+
+  digitalWrite(led, LOW);    // Turn OFF the LED
+  delay(30000);               // Wait for 30 seconds
+}
